e797ee9324c2652da2b1ad32555eba11b6979a01
Wilmer <wilmer@example.com>
2025-09-05 21:48:18 +0000

    fix: Corrección de errores y mejoras visuales en perfiles

--- src/components/CardProfile.js
+++ src/components/CardProfile.js
@@ -1,27 +1,36 @@
-import { getDataProfile } from './getDataPerson';
+import { getDataPersons } from './getDataPerson';
 import React, { useState, useEffect } from 'react';
 import '../css/Profile.css';
 
 function CardProfile() {
-  const [profiles, setProfiles] = useState([]);
+  const [persons, setPersons] = useState([]);
   const [searchTerm, setSearchTerm] = useState('');
+  const [loading, setLoading] = useState(true);
 
   useEffect(() => {
     const fetchData = async () => {
-      const profile = await getDataProfile();
-      setProfiles(profile);
+      const personsData = await getDataPersons();
+      setPersons(personsData);
+      setLoading(false);
     };
 
     fetchData();
   }, []);
 
-  const filteredProfiles = profiles.filter((profile) =>
-    profile.name.toLowerCase().includes(searchTerm.toLowerCase())
-  );
+  const filteredPersons = Array.isArray(persons)
+    ? persons.filter((person) =>
+        person.name.toLowerCase().includes(searchTerm.toLowerCase())
+      )
+    : [];
 
   const handleSearchChange = (e) => {
     setSearchTerm(e.target.value);
   };
+
+  if (loading) {
+    return <div>Cargando...</div>;
+  }
+  // console.log(getDataPersons())
 
   return (
     <div className="profile-container">
@@ -35,7 +44,7 @@ function CardProfile() {
         />
       </div>
       <div className="profile-grid">
-        {filteredProfiles.map(({ id, name, cargo }) => (
+        {filteredPersons.map(({ id, name, cargo }) => (
           <div className="profile-card" key={id}>
             <div className="profile-card-header">
               <h5 className="profile-name">{name}</h5>

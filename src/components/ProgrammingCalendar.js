f7edd2c72345b1b224a1da2a92d6d6b255fa6712
Wilmer <wilmer@example.com>
2025-09-08 05:05:05 +0000

    FixColorHoliday

--- src/components/ProgrammingCalendar.js
+++ src/components/ProgrammingCalendar.js
@@ -2,19 +2,20 @@ import React from 'react';
 import { useCalendar } from '../context/CalendarContext.js';
 
 function ProgrammingCalendar({ date }) {
-  const { people, holidays, assignedDays } = useCalendar();
-//console.log(holidays)
+  const { people, colombianHolidays, assignedDays } = useCalendar();
   const year = date.getFullYear();
-  const month = date.getMonth();
+  const month = date.getMonth(); // 0-11
 
   const firstDayOfMonth = new Date(year, month, 1).getDay();
   const daysInMonth = new Date(year, month + 1, 0).getDate();
 
   const isHoliday = (day) => {
-    if (!holidays) return '';
-    const holiday = holidays.find(h => h.day === day && h.month - 1 === month);
+    if (!colombianHolidays) return '';
+    // Lógica de comparación corregida para el mes (festivo.mes es 1-12, month es 0-11)
+    const holiday = colombianHolidays.find(h => h.dia === day && h.mes === month + 1);
     return holiday ? 'holiday' : '';
   };
+  
 
   const getPeopleForDay = (day) => {
     if (!assignedDays || !people) {
@@ -49,12 +50,13 @@ function ProgrammingCalendar({ date }) {
         <div className="header-pro">Sab</div>
 
         {allCells.map((day, index) => {
+          console.log(isHoliday(day))
           if (!day) {
             return <div className="day-cell-pro empty" key={`empty-${index}`}></div>;
           }
           const peopleOnDay = getPeopleForDay(day);
           return (
-            <div className={`day-cell-pro ${isHoliday(day)}`} key={day}>
+            <div className={`day-cell-pro ${isHoliday(day)}` } key={day} >
               <div className="day-number-pro">{day}</div>
               <div className="people-list-pro">
                 {peopleOnDay.map((name, i) => <div key={i}>{name}</div>)}

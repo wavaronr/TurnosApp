f8eada88180b84698427274443b4e945f7a9ef58
Wilmer <wilmer@example.com>
2025-09-17 02:23:47 +0000

    fix readme

--- .idx/dev.nix
+++ .idx/dev.nix
@@ -1,27 +1,30 @@
-{pkgs}: {
-  channel = "stable-24.05";
-  packages = [
-    pkgs.nodejs_20,
-    pkgs.concurrently,
-    pkgs.mongodb
-  ];
-  idx.extensions = [
-    "svelte.svelte-vscode",
-    "vue.volar"
-  ];
-  idx.previews = {
-    previews = {
-      web = {
-        command = [
-          "sh",
-          "-c",
-          "concurrently \"cd FrontTurnos && npm install && npm start\" \"node BackendTurnos/server.js\""
-        ];
-        env = {
-          PORT = "$PORT";
-          MONGOMS_SYSTEM_BINARY = "${pkgs.mongodb}/bin/mongod";
+{ pkgs, ... }: {
+  # Let Nix handle a few common directories
+  per-folder = {
+    "." = {
+      # The entrypoint for the dev environment.
+      # This is the command that will be run when you start the project.
+      entrypoint = "bash";
+      
+      # Tell VScode to use the dev-provided extensions
+      vscode.extensions.useRecommended = true;
+
+      # The list of processes to run
+      # The key is the process name
+      # The value is the process configuration
+      processes = {
+        dev = {
+          command = [
+            "bash",
+            "-c",
+            "concurrently \"cd FrontTurnos && pnpm install && pnpm start\" \"cd BackendTurnos && pnpm install && pnpm start\""
+          ];
+          env = {
+            PORT = "$PORT";
+            MONGOMS_SYSTEM_BINARY = "${pkgs.mongodb}/bin/mongod";
+          };
+          manager = "web";
         };
-        manager = "web";
       };
     };
   };
